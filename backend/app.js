91bac6124e3d7942962b56cccd6c3d6a3be531ef
Robert Rinehart <rinebob111185@gmail.com>
2019-04-07 17:23:06 -0700

    Lec 37-41

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,48 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+
 app.use((req, res, next) => {
-	console.log('First effing middleware dude!');
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PATCH, PUT, UPDATE, DELETE, OPTIONS"
+	);
 	next();
 });
 
-app.use((req, res, next) => {
-	res.send('Yo from dude')
+app.post("/api/posts", (req, res, next) => {
+	const post = req.body;
+	console.log("000 app.js app.post.  post = ", post);
+	res.status(201).json({
+		message: 'Post added sweet dude!'
+	});
+});
+
+app.get("/api/posts", (req, res, next) => {
+	const posts = [
+		{
+			id: 'werwrewer',
+			title: 'First yo server side',
+			content: 'hey this is from the effin server'
+		},
+		{
+			id: 'sfgsdfgsfdgd',
+			title: 'Second yo server side',
+			content: 'hey dipstick this is from the totally effin server'
+		},
+	];
+	res.status(200).json({
+		message: 'Posts like totally fetched man',
+		posts: posts
+	});
 });
 
 module.exports = app;
