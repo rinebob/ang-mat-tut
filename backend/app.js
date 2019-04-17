32b6d4b446627d6e669356ef19fbdcf84f1fcc91
Bob Rinehart <rinebob111185@gmail.com>
2019-04-16 21:07:50 -0700

    Lec 44 to 54

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+const Post = require('./models/post');
+
+
+dbURI = 'mongodb://localhost/ang-material-tut';
+mongoose.connect( dbURI )
+	.then(() => {
+		console.log("100 app.js Connected to database");
+	})
+	.catch((() => {
+		console.log("101 app.js mongo connection failed")
+	}));
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: false}));
@@ -19,7 +31,12 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-	const post = req.body;
+	const post = new Post({
+		title: req.body.title,
+		content: req.body.content
+	});
+	post.save();
+
 	console.log("000 app.js app.post.  post = ", post);
 	res.status(201).json({
 		message: 'Post added sweet dude!'
@@ -27,22 +44,14 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get("/api/posts", (req, res, next) => {
-	const posts = [
-		{
-			id: 'werwrewer',
-			title: 'First yo server side',
-			content: 'hey this is from the effin server'
-		},
-		{
-			id: 'sfgsdfgsfdgd',
-			title: 'Second yo server side',
-			content: 'hey dipstick this is from the totally effin server'
-		},
-	];
-	res.status(200).json({
-		message: 'Posts like totally fetched man',
-		posts: posts
+	Post.find().then(posts => {
+		console.log('app.js 200 Post.find.  posts = ',posts);
+		res.status(200).json({
+			message: 'Posts like totally fetched man',
+			posts: posts
+		});
 	});
+
 });
 
 module.exports = app;
